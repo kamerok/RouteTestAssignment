ca0fc812526531e9506aec5d4d3589bc14690f5e
Maksim Dvoretskii <kamerok@gmail.com>
2019-04-14 12:50:16 +0300

    Add trim

--- app/src/main/java/com/kamer/aviasalestest/features/select/SelectCityActivity.kt
+++ app/src/main/java/com/kamer/aviasalestest/features/select/SelectCityActivity.kt
@@ -59,7 +59,7 @@ class SelectCityActivity : AppCompatActivity(), TextWatcher {
     override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
 
     override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-        viewModel.postEvent(QueryChanged(queryView.text.toString()))
+        viewModel.postEvent(QueryChanged(queryView.text.toString().trim()))
     }
 
     private fun updateViews(state: SelectCityUiModel) {
